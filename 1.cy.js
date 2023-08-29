1f8dd7dffd9ceb98e3f162b2da0331bd78cdb221
ArutunyanEdgarQA <130232052+ArutunyanEdgarQA@users.noreply.github.com>
2023-08-29 07:57:26 +0400

    Update 1.cy.js

--- 1.cy.js
+++ 1.cy.js
@@ -3,14 +3,14 @@ describe("1. Verify error message for invalid email/password", () => {
     cy.visit("https://app2.abtasty.com/login");
     cy.get('#email', { timeout: 10000 }).should('be.visible');
     cy.get('#email').type('Test@test.test').should('have.value', 'Test@test.test');
-    cy.get('#password').type('password1234');
+    cy.get('#password').type('password1234567890');
     cy.get('#password').should('have.attr', 'type', 'password');
     cy.get('[data-testid="showIcon"]').click();
-    cy.get('#password').should('have.value', 'password1234');
+    cy.get('#password').should('have.value', 'password1234567890');
     cy.get('[data-testid="hideIcon"]').click();
     cy.get('#password').should('have.attr', 'type', 'password');
-    cy.get('.FormButtonRow__buttonRow___vri7h > ._button_1qtjo_184').click();
-    cy.get('.LoginForm__commonError___DSL1P').should('be.visible');
+    cy.get('.FormButtonRow-module__buttonRow___GE1td > ._button_qzvqq_184').click();
+    cy.get('.LoginForm-module__commonError___i23iO').should('be.visible');
     cy.get('#email').clear();
     cy.get('#password').clear();
     // Now we are checking with the correct email and password wich exist in DB
