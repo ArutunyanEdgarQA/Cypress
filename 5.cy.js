752d757f8d26087801712dea43ebb270ad40c7ec
ArutunyanEdgarQA <130232052+ArutunyanEdgarQA@users.noreply.github.com>
2023-08-29 07:59:29 +0400

    Update 5.cy.js

--- 5.cy.js
+++ 5.cy.js
@@ -2,10 +2,10 @@ describe("5. Password Reset - negative", () => {
   it('Password reset"', () => {
     cy.visit("https://app2.abtasty.com/login");
     cy.get('#email', { timeout: 10000 }).should('be.visible');
-    cy.get('.LoginForm__link___qoxwy').click();
-    cy.get('._commonInput_1h7ag_184', { timeout: 10000 }).should('be.visible');
-    cy.get('._commonInput_1h7ag_184').type('test22@test22.test22'); // Email shouldn't exist in DB
-    cy.get('._button_1qtjo_184').click();
+    cy.get('.LoginForm-module__link___oH9Ss').click();
+    cy.get('._commonInput_11efi_184', { timeout: 10000 }).should('be.visible');
+    cy.get('._commonInput_11efi_184').type('test22@test22.test'); // Email shouldn't exist in DB
+    cy.get('._button_qzvqq_184').click();
     cy.contains('There is no such user').should('be.visible'); // Replace this message to valuable
   });
   });
