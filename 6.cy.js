056e65e427426ea499fb101755b54aba5b617248
ArutunyanEdgarQA <130232052+ArutunyanEdgarQA@users.noreply.github.com>
2023-08-29 08:00:06 +0400

    Update 6.cy.js

--- 6.cy.js
+++ 6.cy.js
@@ -4,12 +4,9 @@ describe("6. Verify Captcha activation after 3 failed login attempts", () => {
     cy.get('#email', { timeout: 10000 }).should('be.visible');
     cy.get('#email').type('Test@test.test').should('have.value', 'Test@test.test');
     cy.get('#password').type('password1234');
-    cy.get('.FormButtonRow__buttonRow___vri7h > ._button_1qtjo_184').click();
-    cy.get('.LoginForm__commonError___DSL1P').should('be.visible');
-    cy.get('.FormButtonRow__buttonRow___vri7h > ._button_1qtjo_184').click();
-    cy.get('.LoginForm__commonError___DSL1P').should('be.visible');
-    cy.get('.FormButtonRow__buttonRow___vri7h > ._button_1qtjo_184').click();
-    cy.get('.LoginForm__commonError___DSL1P').should('be.visible');
+    cy.get('.FormButtonRow-module__buttonRow___GE1td > ._button_qzvqq_184').click();
+    cy.get('.FormButtonRow-module__buttonRow___GE1td > ._button_qzvqq_184').click();
+    cy.get('.FormButtonRow-module__buttonRow___GE1td > ._button_qzvqq_184').click();
   });
 
 });
