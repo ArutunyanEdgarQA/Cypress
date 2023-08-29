049f5d5fd9ffcfc200683ae94ad6a93c6482a577
ArutunyanEdgarQA <130232052+ArutunyanEdgarQA@users.noreply.github.com>
2023-08-29 08:03:10 +0400

    Update 6.cy.js

--- 6.cy.js
+++ 6.cy.js
@@ -7,6 +7,7 @@ describe("6. Verify Captcha activation after 3 failed login attempts", () => {
     cy.get('.FormButtonRow-module__buttonRow___GE1td > ._button_qzvqq_184').click();
     cy.get('.FormButtonRow-module__buttonRow___GE1td > ._button_qzvqq_184').click();
     cy.get('.FormButtonRow-module__buttonRow___GE1td > ._button_qzvqq_184').click();
+    cy.get('[style="width: 304px; height: 78px;"] > div > iframe').should('be.visible');
   });
 
 });
